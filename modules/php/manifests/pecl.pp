66ace4962d93b49e1df864f5a6d343fc6bdfe16a
Tom Bevers <t.bevers@gmail.com>
2011-12-31 08:46:59 +0100

    Do not try to install pecl package when its alredy installed

--- modules/php/manifests/pecl.pp
+++ modules/php/manifests/pecl.pp
@@ -2,6 +2,7 @@ class php::pecl {
   include php
   # Install xdebug via PECL
   exec { "pecl install xdebug":
-    require => [Package['php-pear', 'php5-dev'], Exec['pear upgrade']]
+    unless => 'pecl list | grep "xdebug"',
+    require => [Package['php-pear', 'php5-dev'], Exec['pear upgrade']],
   }
 }
